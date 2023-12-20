ebc7d4d3f9115fd97fe8da175c7079b7f6fb0d72
Anh Khoa <binm8.4356@gmail.com>
2023-12-20 22:14:04 +0700

    init project

---
+++ database/migrations/2023_12_20_143904_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('phone', 12);
+            $table->string('data', 10);
+            $table->date('registered_at');
+            $table->date('expired_at');
+            $table->text('available_data')->nullable();
+            $table->string('sales_note')->nullable();
+            $table->string('admin_note')->nullable();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnUpdate()->nullOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
