a9e23c2b0adcb2c58ae8b68f0197e3cfd39d2cdd
Anh Khoa <khoa.nguyen@atech88.com>
2024-01-03 10:26:42 +0700

    config env

--- config/one_sell.php
+++ config/one_sell.php
@@ -2,10 +2,10 @@
 
 return [
 
-    'endpoint' => env('ONESELL_ENDPOINT', 'https://dev.mobishop.vn'),
+    'endpoint' => env('ONESELL_ENDPOINT', 'https://dev.mobishop.vn') ?? 'https://dev.mobishop.vn',
 
-    'apiKey' => env('ONESELL_APIKEY', '4944e44357e08fd0dcd2d6e2c7544c528d2a3a1e881d06d176ecfc5416623cdd'),
+    'apiKey' => env('ONESELL_APIKEY', '4944e44357e08fd0dcd2d6e2c7544c528d2a3a1e881d06d176ecfc5416623cdd') ?? '4944e44357e08fd0dcd2d6e2c7544c528d2a3a1e881d06d176ecfc5416623cdd',
 
-    'source' => env('ONESELL_SOURCE', 'testinfo'),
+    'source' => env('ONESELL_SOURCE', 'testinfo') ?? 'testinfo',
 
 ];
