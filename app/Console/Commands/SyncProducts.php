19b6d90d1d739dc7b3117ccf1b5be2a5c7ccbf72
Anh Khoa <khoa.nguyen@atech88.com>
2024-01-15 11:36:36 +0700

    loading

--- app/Console/Commands/SyncProducts.php
+++ app/Console/Commands/SyncProducts.php
@@ -1,19 +1,37 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Console\Commands;
 
 use App\Helpers\Facades\OneSell;
 use App\Models\Product;
-use Illuminate\Http\Request;
+use Illuminate\Console\Command;
 
-class OneSellController extends Controller
+class SyncProducts extends Command
 {
-    public function __invoke() {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:sync-products';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Sync products from 1sell';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
         // $categories = OneSell::categories('mobifone');
         $insert = [];
         $page = 1;
         do {
-            $products = OneSell::products('mobifone', 51407, null, 1, 10);
+            $products = OneSell::products('mobifone', 51407, null, $page, 10);
             $pageCount = $products['pagination']['pageCount'];
             foreach ($products['data'] as $product) {
                 $insert[] = [
@@ -31,6 +49,5 @@ class OneSellController extends Controller
             $page++;
         } while ($page > $pageCount);
         Product::insert($insert);
-        return response('ok');
     }
 }
