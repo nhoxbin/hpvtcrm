6a3e7e7aab11bcd1bf06cf7dafd69b14366ef63b
Anh Khoa <khoa.nguyen@atech88.com>
2024-01-18 12:59:33 +0700

    loading

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,9 +2,8 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
-use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -22,8 +21,8 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::before(function ($user, $ability) {
+        /* Gate::before(function ($user, $ability) {
             return $user->hasRole('Super Admin') ? true : null;
-        });
+        }); */
     }
 }
