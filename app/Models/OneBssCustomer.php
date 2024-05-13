e4122445d78a7753e803c86c7eb4bae38fd26065
Anh Khoa <khoa.nguyen@atech88.com>
2024-05-13 17:51:23 +0700

    onebss

--- app/Models/OneBssCustomer.php
+++ app/Models/OneBssCustomer.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class OneBssCustomer extends Model
 {
@@ -33,7 +34,10 @@ class OneBssCustomer extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function search($goi_data, $expires_in) : Builder {
+    public function search(Request $request) : Builder {
+        $goi_data = !empty($request->only('goi_data')) ? $request->only('goi_data')['goi_data'] : null;
+        $expires_in = !empty($request->only('expires_in')) ? $request->only('expires_in')['expires_in'] : null;
+        $tra_sau = !empty($request->only('tra_sau')) ? $request->only('tra_sau')['tra_sau'] : null;
         return $this->query()
             ->when($goi_data, function($query, $search) {
                 foreach ($search as $goi_data) {
@@ -46,6 +50,11 @@ class OneBssCustomer extends Model
             ->when($expires_in, function($query, $search) {
                 $query->whereRaw('JSON_UNQUOTE(JSON_EXTRACT(JSON_EXTRACT(`goi_data`, "$[*].TIME_END"), "$[0]")) >= "'.Carbon::now()->format('d/m/Y H:i:s').'"');
                 $query->whereRaw('JSON_UNQUOTE(JSON_EXTRACT(JSON_EXTRACT(`goi_data`, "$[*].TIME_END"), "$[0]")) <= "'.Carbon::now()->addDays($search)->format('d/m/Y H:i:s').'"');
+            })
+            ->when($tra_sau, function($query, $search) {
+                if (count($search) == 1) {
+                    $query->where('tra_sau', $search);
+                }
             })
             ->with(['user'])
             ->orderBy('id', 'asc');
