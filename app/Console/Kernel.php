8b94840781857cc1554435b6410a95164371e2a5
Anh Khoa <khoa.nguyen@atech88.com>
2024-06-17 16:28:44 +0700

    onebss

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command('app:sync-products')->hourly();
         $schedule->command('app:check-digishop-session')->hourly();
-        $schedule->command('app:onebss-check-customers')->everyFiveMinutes()->shouldSkipDueToOverlapping();
+        // $schedule->command('app:onebss-check-customers')->everyFiveMinutes()->shouldSkipDueToOverlapping();
     }
 
     /**
