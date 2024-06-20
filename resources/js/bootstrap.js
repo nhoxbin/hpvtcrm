8cba6a0bdf6b7bfb66521e3f0719ece2c59cd490
Anh Khoa <khoa.nguyen@atech88.com>
2024-06-20 12:12:31 +0700

    onebss

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,7 +8,11 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
+window.onload = function() {
+    document.onselectstart = function() {
+      return false;
+    }
+}
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
