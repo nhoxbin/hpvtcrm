7c17acc3204abc1a35b12dcb2c715a66076873a3
Anh Khoa <khoa.nguyen@atech88.com>
2024-09-19 19:44:43 +0700

    .

--- app/AsyncJobs/DigiShopJob.php
+++ app/AsyncJobs/DigiShopJob.php
@@ -2,14 +2,31 @@
 
 namespace App\AsyncJobs;
 
+use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Artisan;
+use Spatie\Async\Task;
+use VXM\Async\Invocation;
 
-class DigiShopJob
+class DigiShopJob extends Task
 {
-    public function __invoke($job)
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct(private string $job) {}
+
+    public function configure() {}
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function run()
     {
         Artisan::call('queue:work', [
-            '--queue' => $job,
+            '--queue' => $this->job,
             '--once' => true,
             '--tries' => 3,
             '--stop-when-empty' => true
