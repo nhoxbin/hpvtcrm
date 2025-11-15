70c184a59037f09a4d15a792054cdb4ab5b5e51a
BinYet <binm8.4356@gmail.com>
2025-11-15 14:26:16 +0700

    test

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ echo "REMOTE_DIR: $REMOTE_DIR"
 lftp -d -c "
 set ssl:verify-certificate no;
 open -u $FTP_USERNAME,$FTP_PASSWORD $FTP_HOST;
-mirror -R --verbose --only-newer --parallel=10 --exclude-glob .git* --exclude .git/ $LOCAL_DIR $REMOTE_DIR;
+mirror -R --verbose --only-newer --older-than=now-7days --parallel=10 --exclude-glob .git* --exclude .git/ $LOCAL_DIR $REMOTE_DIR;
 bye;
 "
